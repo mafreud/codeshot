2a8dedce05a583f972b452661b0650380daf4c4c
Masahiro Aoki <rafe.kun@gmail.com>
2022-12-03 17:27:05 +0900

    add: bg colors

--- lib/src/features/code_editor/code_editor_page.dart
+++ lib/src/features/code_editor/code_editor_page.dart
@@ -27,6 +27,54 @@ class CodeEditorPage extends ConsumerWidget {
         Color(0xFF6355E8),
       ],
     ),
+    const LinearGradient(
+      begin: Alignment.topLeft,
+      end: Alignment.bottomRight,
+      colors: [
+        Color(0xFF396afc),
+        Color(0xFF2948ff),
+      ],
+    ),
+    const LinearGradient(
+      begin: Alignment.centerLeft,
+      end: Alignment.centerRight,
+      colors: [
+        Color(0xFF1c92d2),
+        Color(0xFFf2fcfe),
+      ],
+    ),
+    const LinearGradient(
+      begin: Alignment.centerLeft,
+      end: Alignment.centerRight,
+      colors: [
+        Color(0xFF0f9b0f),
+        Color(0xFF000000),
+      ],
+    ),
+    const LinearGradient(
+      begin: Alignment.topCenter,
+      end: Alignment.bottomCenter,
+      colors: [
+        Color(0xFFc0392b),
+        Color(0xFF8e44ad),
+      ],
+    ),
+    const LinearGradient(
+      begin: Alignment.topCenter,
+      end: Alignment.bottomCenter,
+      colors: [
+        Color(0xFF159957),
+        Color(0xFF155799),
+      ],
+    ),
+    const LinearGradient(
+      begin: Alignment.topCenter,
+      end: Alignment.bottomCenter,
+      colors: [
+        Color(0xFF000046),
+        Color(0xFF1CB5E0),
+      ],
+    ),
   ];
 
   @override
